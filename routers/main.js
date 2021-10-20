b7d3fc709d7abd9eb4716e3481a31d04b499d1aa
bohoon Lee <bohoon100@gmail.com>
2021-10-20 10:35:06 +0900

    bohoon commit

--- routers/main.js
+++ routers/main.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const authUser = require('../middlewares/authMiddleware');
-const multer = require('multer');
-const path = require('path');
+const express = require("express")
+const router = express.Router()
+const authUser = require("../middlewares/authMiddleware")
+const multer = require("multer")
+const path = require("path")
 
-const { GetMainPosting, CreatePosting } = require('./controller/main');
+const { GetMainPosting, CreatePosting } = require("./controller/main")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../upload'));
+    cb(null, path.join(__dirname, "../upload"))
   },
   filename: function (req, file, cb) {
-    cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
+    cb(null, new Date().valueOf() + path.extname(file.originalname)) // cb 콜백함수를 통해 전송된 파일 이름 설정
   },
-});
-const upload = multer({ storage: storage });
+})
+const upload = multer({ storage: storage })
 
 router
-  .route('/')
+  .route("/view")
   // 게시물 전체 불러오기
-  //   .get(authUser, GetMainPosting)
+  .get(authUser, GetMainPosting)
+router
+  .route("/post")
   //게시물 작성
-  .post(authUser, upload.single('image'), CreatePosting);
+  .post(authUser, upload.single("image"), CreatePosting)
 
-module.exports = router;
+module.exports = router
--- schema/users.js
+++ schema/users.js
@@ -1,29 +1,28 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
 
-const UsersSchema = new Schema(
-  {
-    nickname: {
-      type: String,
-      required: true,
-      unique: true,
-      min: 3,
-    },
-    userId: {
-      type: String,
-      required: true,
-      unique: true,
-      min: 3,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 4,
-    },
+const UsersSchema = new Schema({
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 3,
   },
-  {
-    versionKey: false,
-  }
-);
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 4,
+  },
+}, {
+  versionKey: false,
+});
 
 module.exports = mongoose.model('Users', UsersSchema);
