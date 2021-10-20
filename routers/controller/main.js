7010c18db5ef0aafdeb8a3b19c24aaf730e76023
Jaehyoen park <parkjh9370@naver.com>
2021-10-20 10:37:01 +0900

    [fix]post

--- routers/controller/main.js
+++ routers/controller/main.js
@@ -1,7 +1,7 @@
 // const Joi = require('joi');
-const Users = require('../../schema/users');
-const Postings = require('../../schema/postings');
-const printError = require('../../library/error');
+const Users = require("../../schema/users");
+const Postings = require("../../schema/postings");
+const printError = require("../../library/error");
 
 GetMainPosting = async (req, res, next) => {
   try {
@@ -17,19 +17,19 @@ GetMainPosting = async (req, res, next) => {
 
 CreatePosting = async (req, res, next) => {
   try {
-    console.log('트라이들어옴');
     // Joi 검증 필요한지 확인하자??
     //ID로 찾아서 nickname을 가져올 지 아니면
     const userId = res.locals.user; //유저정보가져오기위한 수단 날짜생성
-    console.log(userId);
     const Finduser = await Users.findOne({ userId: userId });
-    console.log(Finduser.nickname); //작성자 닉네임 가져오기
+    console.log(Finduser.nickname);
 
+    const nickname = Finduser.nickname;
+    const image = req.file.filename;
     const { text, image, createdAt } = req.body;
     await Postings.create({ nickname, text, image, comment, like });
-    res.send({ result: 'success', msg: '게시글 작성에 성공했습니다.' });
+    res.send({ result: "success", msg: "게시글 작성에 성공했습니다." });
   } catch (err) {
-    res.send({ result: 'fail', msg: '게시글 작성에 실패했습니다.' });
+    res.send({ result: "fail", msg: "게시글 작성에 실패했습니다." });
 
     printError(req, err);
     next();
